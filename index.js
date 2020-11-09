63f0143a11109ed4074bf925eafc3d5169b1af14
patel <parthpatel167@gmail.com>
2020-11-09 17:53:19 -0600

    question updated

--- index.js
+++ index.js
@@ -1,7 +1,59 @@
 // array of questions for user
-const questions = [
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generate = require('./utils/generateMarkdown');
 
+// array of questions for user
+const questions = [
+    {
+        type: 'input',
+        message: 'Type your GitHub username:',
+        name: 'github'
+    },
+    {
+        type: 'input',
+        message: 'Type your email address:',
+        name: 'Email'
+    },
+    {
+        type: 'input',
+        message: 'What is the title of the project?',
+        name: 'Title'
+    },
+    {
+        type: 'input',
+        message: 'Type a description of the project:',
+        name: 'Description'
+    },
+    {
+        type: 'input',
+        message: 'Type instructions on how to install the project:',
+        name: 'Install'
+    },
+    {
+        type: 'input',
+        message: 'Type instructions on how to use the project:',
+        name: 'Usage'
+    },
+    {
+        type: 'list',
+        message: 'Choose a license:',
+        name: 'License',
+        choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None']
+    },
+    {
+        type: 'input',
+        message: 'Type how to contribute to the project:',
+        name: 'Contribute'
+    },
+    {
+        type: 'input',
+        message: 'Type how to test the project:',
+        name: 'Test'
+    },
+    
 ];
+
 
 // function to write README file
 function writeToFile(fileName, data) {
