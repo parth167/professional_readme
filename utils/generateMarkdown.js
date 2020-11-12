777bd4964087f549b24135ddda499d9dc8f12412
patel <parthpatel167@gmail.com>
2020-11-11 21:20:59 -0600

    readme file updated with plate

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,47 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+#  ${data.Title}
 
+${data.badge}
+<br/>
+${data.Description}
+<br/>
+<br/>
+## Table of Contents
+* [Installation](#Installation)
+* [Usage](#Usage)
+* [License](#License)
+* [Contributing](#Contributing)
+* [Tests](#Tests)
+* [Questions](#Questions)
+<br/>
+<br/>
+## Installation
+${data.Install}
+<br/>
+<br/>
+## Usage
+${data.Usage}
+<br/>
+<br/>
+## License
+[${data.License}](${data.URL})
+<br/>
+<br/>
+## Contributing
+${data.Contribute}
+<br/>
+<br/>
+## Tests
+${data.Test}
+<br/>
+<br/>
+## Questions  
+If you have questions you can send me a message in GitHub or to my email address.  
+My GitHub profile is [${data.GitHub}](https://github.com/${data.GitHub})  
+  
+Email: ${data.Email}.  
 `;
 }
 
