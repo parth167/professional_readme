a538b48a260f5d682e288c321b9504d7429186fb
patel <parthpatel167@gmail.com>
2020-11-12 13:15:54 -0600

    few changes done,licence badge is working now

--- index.js
+++ index.js
@@ -8,12 +8,12 @@ const questions =[
     {
         type: 'input',
         message: 'Type your GitHub username:',
-        name: 'Github'
+        name: 'github'
     },
     {
         type: 'input',
         message: 'Type your email address:',
-        name: 'Email'
+        name: 'email'
     },
     {
         type: 'input',
@@ -67,17 +67,17 @@ function writeToFile(data) {
 async function getUserInput() {
     let response = await inquirer.prompt(questions);
     if (response) {
-        if(response.projectLicense === 'MIT'){
+        if(response.License === 'MIT'){
             response.badge = '![MIT license](https://img.shields.io/badge/license-MIT-green)';
-            response.licenseURL = 'https://choosealicense.com/licenses/mit/';
+            response.licenseurl = 'https://choosealicense.com/licenses/mit/';
         };
-        if(response.projectLicense === 'Apache License 2.0'){
+        if(response.License === 'Apache License 2.0'){
             response.badge ='![Apache 2.0 license](https://img.shields.io/badge/license-Apache%202.0-blue)';
-            response.licenseURL = 'https://choosealicense.com/licenses/apache-2.0/';
+            response.licenseurl = 'https://choosealicense.com/licenses/apache-2.0/';
         };
-        if(response.projectLicense === 'GPL 3'){
+        if(response.License === 'GPL 3'){
             response.badge ='![GPL 3 license](https://img.shields.io/badge/license-GNU%20GPLv3-brightgreen)';
-            response.licenseURL = 'https://choosealicense.com/licenses/gpl-3.0/';
+            response.licenseurl = 'https://choosealicense.com/licenses/gpl-3.0/';
         };
         writeToFile(generate(response));
     }
